615c340f922dd29a23df27ff84b2a21fc14de3c8
Moilamar <h8718@student.jamk.fi>
2017-10-22 18:24:20 +0300

    Before testing

---
+++ src/components/PosterListElement.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-ons-list-item @click="openInfoPage" class="search-list-item">
+    <div class="left portrait-small">
+      <img v-if="movie.poster_path" class="list-item__thumbnail" :src="imageUrl">
+      <!-- Show placeholder if no movie poster -->
+      <img v-else class="list-item__thumbnail" src="../media/placeholder.jpg"> 
+    </div>
+    <div class="center">
+      <ons-col width="47vw">
+        <span class="list-item__title">{{ shortTitle }}
+          <span class="list-year">({{ releaseYear }})</span>
+        </span>
+        <ons-row><span class="list-item__subtitle">{{ genreNames }}</span></ons-row>
+      </ons-col>
+      <ons-col style="marginLeft:1vw;">
+        <ons-row>
+          <ons-icon icon="fa-star" size="5vw"></ons-icon>
+          <span class="list-score">{{ movie.vote_average }}</span>
+          
+        </ons-row>
+      </ons-col>
+    </div>
+    <div class="right">
+      <div class="circle"><ons-icon icon="ion-chevron-right" size="3.1vw"></ons-icon></div>
+    </div>
+  </v-ons-list-item>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      genreNames: "",
+      genres: this.$store.state.genres
+    }
+  },
+  mounted() {
+    this.getGenreNames();
+  },
+  computed: {
+    releaseYear: function() {
+      return this.movie.release_date.substring(0,4);
+    },
+    imageUrl: function() {
+      return "http://image.tmdb.org/t/p/w92/"+this.movie.poster_path;
+    },
+    shortTitle: function() {
+      return this.movie.title.length < 25 ? this.movie.title : this.movie.title.substring(0,23) + "...";
+    }
+  },
+  methods: {
+    getGenreNames() { // Transform genre ids into names
+      for(var i=0; i<this.movie.genre_ids.length; i++) {
+        this.genreNames += this.genres[this.movie.genre_ids[i]] + ", ";
+        if (i == 2) break;
+      }
+      this.genreNames = this.genreNames.substring(0, this.genreNames.length - 2);
+    },
+    openInfoPage() {
+      this.$store.commit('setMovie', this.movie);
+      this.$store.commit('pushPageStack', 1);
+      return;
+    }
+  },
+  props: [ 'movie' ]
+}
+</script>
