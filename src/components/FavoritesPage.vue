75f7ecf42556e3d5999c27f9d35486111db6ce62
Moilamar <h8718@student.jamk.fi>
2017-10-22 22:27:53 +0300

    Added favorite removal

--- src/components/FavoritesPage.vue
+++ src/components/FavoritesPage.vue
@@ -1,30 +1,28 @@
 <template>
   <v-ons-page>
     <custom-toolbar :back-label="'Back'">Favorites</custom-toolbar>
-    <search-list-element v-for="(movie, index) in favorites" 
-      :movie="movie" :key="index">
-    </search-list-element>
+    <favorite-list-element v-for="(movie, index) in favorites" :movie="movie" :key="index">
+    </favorite-list-element>
   </v-ons-page>
 </template>
 
 <script>
-  import customToolbar from './CustomToolbar';
-  import searchListElement from './SearchListElement';
+import customToolbar from './CustomToolbar';
+import favoriteListElement from './FavoriteListElement';
 
-  export default {
-    data() {
-      const favorites = [];
-      this.$localStorage.get('watchedMovies').forEach(function(movie) {
-        if (movie.favorite) favorites.push(movie);
-      });
-
-      return {
-         favorites
-      }
-    },
-    methods: {
-      
-    },
-    components: { customToolbar, searchListElement }
-  }
+export default {
+  data() {
+    const favorites = [];
+    this.$localStorage.get('watchedMovies').forEach(function(movie) {
+      if (movie.favorite) favorites.push(movie);
+    });
+    return {
+        favorites
+    }
+  },
+  methods: {
+    
+  },
+  components: { customToolbar, favoriteListElement }
+}
 </script>
